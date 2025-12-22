354d47074808d881ce9d38eb61c0a914ccde27ea
Jamshaa <125591505+Jamshaa@users.noreply.github.com>
2025-12-22 08:56:05 +0100

    Add files via upload

---
+++ Diseny-Proves-Unitaries-main/Diseny-Proves-Unitaries/src/main/data/ePrescripCodeException.java
@@ -0,0 +1,12 @@
+package data;
+
+public class ePrescripCodeException extends Exception {
+
+    public ePrescripCodeException(String message) {
+        super(message);
+    }
+
+    public ePrescripCodeException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ Diseny-Proves-Unitaries-main/Diseny-Proves-Unitaries/src/main/services/DecisionMakingAI.java
@@ -0,0 +1,14 @@
+package services;
+
+import java.util.List;
+
+public interface DecisionMakingAI {
+/*
+    void initDecisionMakingIA() throws AIException;
+
+    String getSuggestions(String prompt) throws BadPromptException;
+
+    List<Suggestion> parseSuggest(String aiAnswer);
+
+ */
+}
