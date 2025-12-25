27c9e4ee2f6c2bdaf105171152fd8295354e5f30
alfemi <alefermim@gmail.com>
2025-12-25 23:43:42 +0100

    CreatePosologyTest correct

---
+++ test/medicalconsultation/CreatePosologyTest.java
@@ -0,0 +1,41 @@
+package medicalconsultation;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CreatePosologyTest {
+
+    private Posology posology;
+
+    @BeforeEach
+    void setUp() {
+        posology = new Posology(1.0f, 24.0f, FqUnit.HOUR);
+    }
+
+    @Test
+    void posologyCorrectlyCreatedTest() {
+        assertEquals(1.0f, posology.getDose(), 0.0001);
+        assertEquals(24.0f, posology.getFreq(), 0.0001);
+        assertEquals(FqUnit.HOUR, posology.getFreqUnit());
+    }
+
+    @Test
+    void setDoseTest() {
+        posology.setDose(2.5f);
+        assertEquals(2.5f, posology.getDose(), 0.0001);
+    }
+
+    @Test
+    void setFreqTest() {
+        posology.setFreq(2.0f);
+        assertEquals(2.0f, posology.getFreq(), 0.0001);
+    }
+
+    @Test
+    void setFreqUnitTest() {
+        posology.setFreqUnit(FqUnit.DAY);
+        assertEquals(FqUnit.DAY, posology.getFreqUnit());
+    }
+}
